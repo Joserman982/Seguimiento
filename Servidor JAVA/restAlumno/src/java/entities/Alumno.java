25748be00f3ef0339080228aa3e49804184f9edc
Joserman982 <37843246+Joserman982@users.noreply.github.com>
2019-06-20 11:38:03 -0500

    Actualizacion Proyectos JAVA

---
+++ Servidor JAVA/restAlumno/src/java/entities/Alumno.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rober
+ */
+@Entity
+@Table(name = "alumno")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumno.findAll", query = "SELECT a FROM Alumno a")
+    , @NamedQuery(name = "Alumno.findByIdAlumno", query = "SELECT a FROM Alumno a WHERE a.idAlumno = :idAlumno")
+    , @NamedQuery(name = "Alumno.findByNombre", query = "SELECT a FROM Alumno a WHERE a.nombre = :nombre")
+    , @NamedQuery(name = "Alumno.findByApPaterno", query = "SELECT a FROM Alumno a WHERE a.apPaterno = :apPaterno")
+    , @NamedQuery(name = "Alumno.findByApMaterno", query = "SELECT a FROM Alumno a WHERE a.apMaterno = :apMaterno")
+    , @NamedQuery(name = "Alumno.findBySexo", query = "SELECT a FROM Alumno a WHERE a.sexo = :sexo")
+    , @NamedQuery(name = "Alumno.findByNumero", query = "SELECT a FROM Alumno a WHERE a.numero = :numero")
+    , @NamedQuery(name = "Alumno.findByEmail", query = "SELECT a FROM Alumno a WHERE a.email = :email")})
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_alumno")
+    private Integer idAlumno;
+    @Size(max = 40)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 40)
+    @Column(name = "apPaterno")
+    private String apPaterno;
+    @Size(max = 40)
+    @Column(name = "apMaterno")
+    private String apMaterno;
+    @Column(name = "sexo")
+    private Character sexo;
+    @Size(max = 10)
+    @Column(name = "numero")
+    private String numero;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 70)
+    @Column(name = "email")
+    private String email;
+    @JoinColumn(name = "id_direccion", referencedColumnName = "id_direccion")
+    @ManyToOne
+    private Direccion idDireccion;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public Integer getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Integer idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApPaterno() {
+        return apPaterno;
+    }
+
+    public void setApPaterno(String apPaterno) {
+        this.apPaterno = apPaterno;
+    }
+
+    public String getApMaterno() {
+        return apMaterno;
+    }
+
+    public void setApMaterno(String apMaterno) {
+        this.apMaterno = apMaterno;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Direccion getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(Direccion idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idAlumno != null ? idAlumno.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumno)) {
+            return false;
+        }
+        Alumno other = (Alumno) object;
+        if ((this.idAlumno == null && other.idAlumno != null) || (this.idAlumno != null && !this.idAlumno.equals(other.idAlumno))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Alumno[ idAlumno=" + idAlumno + " ]";
+    }
+    
+}
---
+++ Servidor JAVA/restAlumno/src/java/entities/Direccion.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author rober
+ */
+@Entity
+@Table(name = "direccion")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Direccion.findAll", query = "SELECT d FROM Direccion d")
+    , @NamedQuery(name = "Direccion.findByIdDireccion", query = "SELECT d FROM Direccion d WHERE d.idDireccion = :idDireccion")
+    , @NamedQuery(name = "Direccion.findByCalle", query = "SELECT d FROM Direccion d WHERE d.calle = :calle")
+    , @NamedQuery(name = "Direccion.findByNumeroInt", query = "SELECT d FROM Direccion d WHERE d.numeroInt = :numeroInt")
+    , @NamedQuery(name = "Direccion.findByNumeroExt", query = "SELECT d FROM Direccion d WHERE d.numeroExt = :numeroExt")
+    , @NamedQuery(name = "Direccion.findByColonia", query = "SELECT d FROM Direccion d WHERE d.colonia = :colonia")
+    , @NamedQuery(name = "Direccion.findByCiudad", query = "SELECT d FROM Direccion d WHERE d.ciudad = :ciudad")
+    , @NamedQuery(name = "Direccion.findByEstado", query = "SELECT d FROM Direccion d WHERE d.estado = :estado")
+    , @NamedQuery(name = "Direccion.findByCp", query = "SELECT d FROM Direccion d WHERE d.cp = :cp")})
+public class Direccion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_direccion")
+    private Integer idDireccion;
+    @Size(max = 20)
+    @Column(name = "calle")
+    private String calle;
+    @Size(max = 5)
+    @Column(name = "numeroInt")
+    private String numeroInt;
+    @Size(max = 5)
+    @Column(name = "numeroExt")
+    private String numeroExt;
+    @Size(max = 40)
+    @Column(name = "colonia")
+    private String colonia;
+    @Size(max = 50)
+    @Column(name = "ciudad")
+    private String ciudad;
+    @Size(max = 40)
+    @Column(name = "estado")
+    private String estado;
+    @Size(max = 5)
+    @Column(name = "cp")
+    private String cp;
+    @OneToMany(mappedBy = "idDireccion")
+    private Collection<Alumno> alumnoCollection;
+    @OneToMany(mappedBy = "idDireccion")
+    private Collection<Empresa> empresaCollection;
+
+    public Direccion() {
+    }
+
+    public Direccion(Integer idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public Integer getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(Integer idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumeroInt() {
+        return numeroInt;
+    }
+
+    public void setNumeroInt(String numeroInt) {
+        this.numeroInt = numeroInt;
+    }
+
+    public String getNumeroExt() {
+        return numeroExt;
+    }
+
+    public void setNumeroExt(String numeroExt) {
+        this.numeroExt = numeroExt;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+
+    @XmlTransient
+    public Collection<Alumno> getAlumnoCollection() {
+        return alumnoCollection;
+    }
+
+    public void setAlumnoCollection(Collection<Alumno> alumnoCollection) {
+        this.alumnoCollection = alumnoCollection;
+    }
+
+    @XmlTransient
+    public Collection<Empresa> getEmpresaCollection() {
+        return empresaCollection;
+    }
+
+    public void setEmpresaCollection(Collection<Empresa> empresaCollection) {
+        this.empresaCollection = empresaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idDireccion != null ? idDireccion.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Direccion)) {
+            return false;
+        }
+        Direccion other = (Direccion) object;
+        if ((this.idDireccion == null && other.idDireccion != null) || (this.idDireccion != null && !this.idDireccion.equals(other.idDireccion))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Direccion[ idDireccion=" + idDireccion + " ]";
+    }
+    
+}
