7779b93a34663f2ab2ddf06378939afd42ebb400
deyvid1234 <37844597+deyvid1234@users.noreply.github.com>
2019-06-20 11:23:30 -0500

    Java class Methods

---
+++ Java/IndicadoresUPP/src/usuariosentities/Empresa.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuariosentities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author rober
+ */
+@Entity
+@Table(name = "empresa")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Empresa.findAll", query = "SELECT e FROM Empresa e")
+    , @NamedQuery(name = "Empresa.findByIdEmpresa", query = "SELECT e FROM Empresa e WHERE e.idEmpresa = :idEmpresa")
+    , @NamedQuery(name = "Empresa.findByNombre", query = "SELECT e FROM Empresa e WHERE e.nombre = :nombre")
+    , @NamedQuery(name = "Empresa.findByRfc", query = "SELECT e FROM Empresa e WHERE e.rfc = :rfc")})
+public class Empresa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_empresa")
+    private Integer idEmpresa;
+    @Size(max = 40)
+    @Column(name = "nombre")
+    private String nombre;
+    @Size(max = 13)
+    @Column(name = "rfc")
+    private String rfc;
+    @OneToMany(mappedBy = "idEmpresa")
+    private Collection<Empleador> empleadorCollection;
+    @JoinColumn(name = "id_direccion", referencedColumnName = "id_direccion")
+    @ManyToOne
+    private Direccion idDireccion;
+
+    public Empresa() {
+    }
+
+    public Empresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public Integer getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(Integer idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    @XmlTransient
+    public Collection<Empleador> getEmpleadorCollection() {
+        return empleadorCollection;
+    }
+
+    public void setEmpleadorCollection(Collection<Empleador> empleadorCollection) {
+        this.empleadorCollection = empleadorCollection;
+    }
+
+    public Direccion getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(Direccion idDireccion) {
+        this.idDireccion = idDireccion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEmpresa != null ? idEmpresa.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Empresa)) {
+            return false;
+        }
+        Empresa other = (Empresa) object;
+        if ((this.idEmpresa == null && other.idEmpresa != null) || (this.idEmpresa != null && !this.idEmpresa.equals(other.idEmpresa))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "usuariosentities.Empresa[ idEmpresa=" + idEmpresa + " ]";
+    }
+    
+}
